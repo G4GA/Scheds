4dd2e871326f7ef1dcef542afba980e00601ff62
G4GA <Damian.Garcia@toshibagcs.com>
2024-03-06 10:47:53 -0600

    finished

--- fcfs/sched.py
+++ fcfs/sched.py
@@ -14,7 +14,7 @@ class SchedulerFCFS:
     This class will simulate a FCFS scheduler
     using the multiprocessing module
     """
-    def __init__(self, arg_dict=None):
+    def __init__(self, arg_dict=None) -> None:
         self.__settings = {
             'p_count': cpu_count(),
             'p_queue': Queue(),
@@ -25,46 +25,48 @@ class SchedulerFCFS:
                 self.p_count = arg_dict['p_count']
 
     @property
-    def cur_p(self):
+    def cur_p(self) -> dict:
         return self.__settings['cur_p']
 
     @cur_p.setter
-    def cur_p(self, cur_p:dict):
+    def cur_p(self, cur_p:dict) -> None:
         if isinstance(cur_p, dict):
             self.__settings['cur_p'] = cur_p
 
-    def set_process_queue(self):
+    def set_process_queue(self) -> None:
         for _ in range(self.p_count):
             p_value = Value('I', 0)
+            p_halt = Value('b', False)
             p_finished = randint(1_500, 3_000)
             p_dict = {
                 'cur_state': p_value,
                 'p_fi_val': p_finished,
-                'process': Process(target=self._dummy_process, args=(p_value, p_finished))
+                'p_halt': p_halt,
+                'process': Process(target=self._dummy_process, args=(p_value, p_finished, p_halt))
             }
             self.p_queue.put(p_dict)
         self.p_queue.put('STOP')
 
-    def run_queue(self):
-        self.cur_p = self._p_queue.get()
+    def run_queue(self) -> None:
+        self.cur_p = self.p_queue.get()
         while self.cur_p != 'STOP':
             self.cur_p['process'].start()
             self.cur_p['process'].join()
             self.cur_p = self.p_queue.get()
 
     @staticmethod
-    def _dummy_process(cur_state, final_state:int):
+    def _dummy_process(cur_state, final_state:int, halt) -> None:
         while cur_state.value < final_state:
-            cur_state.value += 1
-            sleep(randint(0, 100) / 300)
-
+            if not halt.value:
+                cur_state.value += 1
+                sleep(randint(0, 100) / 6_500)
 
     @property
     def p_count(self) -> int:
         return self.__settings['p_count']
 
     @p_count.setter
-    def p_count(self, process_count: int):
+    def p_count(self, process_count: int) -> None:
         if isinstance(process_count, int) and process_count > 0:
             self.__settings['p_count'] = process_count
         else:
--- gui/win.py
+++ gui/win.py
@@ -1,16 +1,20 @@
 """
 This module will store the window class
 """
+from threading import Thread
+
 from PyQt6.QtCore import QTimer
 from PyQt6.QtWidgets import (
     QMainWindow,
     QLabel,
     QWidget,
     QHBoxLayout,
-    QVBoxLayout
+    QVBoxLayout,
+    QPushButton,
+    QProgressBar
 )
-
-from copy import deepcopy
+from fcfs.sched import SchedulerFCFS
+from PyQt6.QtCore import Qt
 
 class MainWindow (QMainWindow):
     """Main window class
@@ -20,24 +24,123 @@ class MainWindow (QMainWindow):
     """
     def __init__(self):
         super().__init__()
-        self.setFixedSize(1_500, 500)
-
-        self.main_widgets = {
+        self.setFixedSize(1_500, 300)
+        self.timer = QTimer()
+        self._main_widgets = {
             'upper': None,
             'lower': None,
         }
+        self._c_ss_lst = [
+            'yellow;',
+            'green;',
+            '#0a7091;'
+        ]
+        self._bttn_dict = {
+            'halt': QPushButton('Halt'),
+            'resume': QPushButton('Resume'),
+        }
+        self.sched = SchedulerFCFS({'p_count':15})
+
         self._set_main_widgets()
-        self.set_main_w_color()
+        self._set_main_w_color()
+
+        self._set_buttons()
+        self._set_labels()
+        self._prog_bar_list = self._set_progbar_lst(15)
+        self._add_pb_lst()
+        self.thread_p = Thread(target=self.sched.run_queue)
+        self.thread_p.start()
+        self.timer.timeout.connect(self.update_state_bar)
+        self.timer.start(100)
 
         central_widget = QWidget()
         self.setCentralWidget(central_widget)
+
+    def update_state_bar(self):
+        for bar, p_dict in self._prog_bar_list:
+            bar.setValue(p_dict['cur_state'].value)
+            self.update_bar_color(bar, p_dict)
+
+    def update_bar_color(self, bar, p_dict):
+        if p_dict['cur_state'].value < p_dict['p_fi_val']:
+                if p_dict['p_halt'].value:
+                    bar.setStyleSheet(f'QProgressBar::chunk {{ background-color: {self._c_ss_lst[0]};}} ')
+                else:
+                    bar.setStyleSheet(f'QProgressBar::chunk {{ background-color: {self._c_ss_lst[1]};}} ')
+        else:
+            bar.setStyleSheet(f'QProgressBar::chunk {{ background-color: {self._c_ss_lst[2]};}} ')
+
+    def _add_pb_lst(self):
+        for bar, _ in self._prog_bar_list:
+            self._main_widgets['upper']['layout'].addWidget(bar)
+
+    def _set_progbar_lst(self, amount):
+        prg_barls = []
+        self.sched.set_process_queue()
+        for _ in range(amount):
+            p_info_dict = self.sched.p_queue.get()
+
+            bar = QProgressBar()
+            bar.setRange(0, p_info_dict['p_fi_val'])
+            bar.setOrientation(Qt.Orientation.Vertical)
+            bar.setStyleSheet('QProgressBar {{border-radius: 10px;}}')
+
+            self.sched.p_queue.put(p_info_dict)
+            prg_barls.append((bar, p_info_dict))
+
+        stp_str = self.sched.p_queue.get()
+        self.sched.p_queue.put(stp_str)
+
+        return prg_barls
+
+    def _set_labels(self):
+        label_container = QWidget()
+        lbl_cont_layout = QHBoxLayout()
+        label_container.setLayout(lbl_cont_layout)
+        label_container.setFixedSize(300, 35)
+
+        state_label = QLabel('State:')
+        lbl_cont_layout.addWidget(state_label)
+
+        self.spawn_label_list(lbl_cont_layout)
+
+        self._main_widgets['lower']['layout'].addWidget(label_container)
+
+    def spawn_label_list(self, layout):
+        lbl_lst = [
+            QLabel('Bloqued'),
+            QLabel('Running'),
+            QLabel('Finished')
+        ]
+        for index, label in enumerate(lbl_lst):
+            label.setStyleSheet(f'color: {self._c_ss_lst[index]};')
+            layout.addWidget(label)
+
+    def _set_buttons(self):
+        for value in self._bttn_dict.values():
+            self._configure_bttns(value)
+        self._configure_click_events()
+
+    def _configure_click_events(self):
+        self._bttn_dict['halt'].clicked.connect(self._halt_process)
+        self._bttn_dict['resume'].clicked.connect(self._resume_process)
+
+    def _configure_bttns(self, bttn):
+        bttn.setStyleSheet('background-color: #72a19f;')
+        self._main_widgets['lower']['layout'].addWidget(bttn)
+
+    def _halt_process(self):
+        self.sched.cur_p['p_halt'].value = True
+
+    def _resume_process(self):
+        self.sched.cur_p['p_halt'].value = False
 
     def setCentralWidget(self, widget: QWidget | None) -> None:
         central_layout = QVBoxLayout()
 
         widget.setLayout(central_layout)
-        central_layout.addWidget(self.main_widgets['upper']['widget'])
-        central_layout.addWidget(self.main_widgets['lower']['widget'])
+        central_layout.addWidget(self._main_widgets['upper']['widget'])
+        central_layout.addWidget(self._main_widgets['lower']['widget'])
 
         super().setCentralWidget(widget)
 
@@ -45,17 +148,17 @@ class MainWindow (QMainWindow):
         """
         Sets window main widgets with their layouts
         """
-        for key, _ in self.main_widgets.items():
+        for key in self._main_widgets.keys():
             widget_dict = {
                 'widget':QWidget(),
-                'layout':QHBoxLayout()
+                'layout':QHBoxLayout() if key == 'upper' else QVBoxLayout()
             }
             widget_dict['widget'].setLayout(widget_dict['layout'])
             widget_dict
-            self.main_widgets[key] = widget_dict
+            self._main_widgets[key] = widget_dict
 
-    def set_main_w_color(self):
-        self.main_widgets['upper']['widget'].setStyleSheet("""background-color: gray;
+    def _set_main_w_color(self):
+        self._main_widgets['upper']['widget'].setStyleSheet("""background-color: gray;
                                                               border-radius: 10px""")
-        self.main_widgets['lower']['widget'].setStyleSheet("""background-color: lightgray;
+        self._main_widgets['lower']['widget'].setStyleSheet("""background-color: darkgray;
                                                               border-radius: 10px""")
